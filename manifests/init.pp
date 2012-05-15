b0953694f4bd8ca799fca79b98758fc78868307a
Jared Curtis <jcurtis@ncircle.com>
2012-05-15 13:26:47 -0700

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: iproute2
+#
+# This module manages iproute2
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class iproute2 {
+
+
+}
